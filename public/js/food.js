76cab932fa9b992ecedab85e82a5a931f6850372
FranCali <fr.califano@gmail.com>
2019-05-04 11:54:53 +0200

    fixed paths

--- public/js/food.js
+++ public/js/food.js
@@ -1,3 +1,16 @@
-module.prova = function(res) {
-    res.send("hello Domenico!!!");
-  }
+var request = require('request');
+const NUTRIDIGM_SUBSCRIPTION_ID = "MyID"
+
+exports.sayHello = function() {
+    console.log("HelloFood");
+}
+
+
+//Retrieve all foods from the nutridigm API
+function getFoodItems(){
+  request('https://api.nutridigm.com/api/v1/nutridigm/fooditems?subscriptionId=MyID', {json : true}, function(error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the response body
+  });
+}

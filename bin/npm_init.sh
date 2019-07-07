c755874fb8cfcc548d3bdd22b0e0b15db5eb1d48
FranCali <fr.califano@gmail.com>
2019-07-07 18:27:02 +0200

    modified script npm init

--- bin/npm_init.sh
+++ bin/npm_init.sh
@@ -3,12 +3,14 @@
 echo "installing npm dependencies..."
 npm install compression --save
 npm install express --save
+npm install fs --save
+npm install languagedetect --save
 npm install mongodb --save
 npm install node-schedule --save
+npm install noodlejs --save
+npm install object-array-converter --save
 npm install request --save
 npm install sync-request --save
 npm install youtube-search --save
-npm install languagedetect --save
-npm install fs --save
 echo "installation completed"
 

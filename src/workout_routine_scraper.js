dd8a9dd7a960caac4c1321ad4ca6f821313cda0e
FranCali <fr.califano@gmail.com>
2019-07-09 17:04:36 +0200

    created woutRoutine objects by scraping

--- src/workout_routine_scraper.js
+++ src/workout_routine_scraper.js
@@ -1,26 +1,53 @@
 const cheerio = require('cheerio');
 const request = require('request');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-request('https://www.workout-routine.com/workout-routines.html', function(error, response, body) {
-    if(error)
-        console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode);
+const SITE = 'www.workout-routine.com'
+
+const equipment = ['dumbbells', 'barbell', 'gym', 'ball', 'bands'];
+const muscularGroups = ['hot-abs', 'strong-back', "large-chest", 'broad-shoulders', 'ripped-arms', 'defined-legs'];
+
+for(var i=0; i < equipment.length; i++){
+    for(var j=0; j < muscularGroups.length; j++){
+        var url = 'https://' + SITE + '/' + equipment[i] + "/" + muscularGroups[j];
+
+        request(url, function(error, response, body) {
+            if(error)
+                console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            if(response.statusCode != 404){
+                $ = cheerio.load(body);
+            
+                var woutRoutine = {};
+                woutRoutine['title'] = cleanText('title', $('title').text());
+                woutRoutine['description'] = cleanText('description', $('p').first().text());
+                woutRoutine['muscularGroup'] = muscularGroups[j];
+                woutRoutine['equipment'] = equipment[i];
+                woutRoutine['routine'] = $('.tableExercise').html(); //Modo becero, devo prendere singolarmente gli esercizi
+
+                addToCollection(woutRoutine);
+
+                console.log(woutRoutine);
+            }
+
+        });
+    }
+}
+
+function cleanText(type, text){
+
+    if(type == 'title')
+        return text.split(" | ")[0];
+    else
+        return text.replace(/[\n\t]/g, '');
     
-    const $ = cheerio.load(body);
-    var selector = 'body'
+}
 
-    const muscularGroups = ['Hot Abs', 'Strong Back', "Large Chest", 'Broad Shoulders', 'Ripped Arms', 'Defined Legs'];
-    const equipment = ['dumbbells', 'barbell', 'gym', 'ball', 'bands'];
-    
-    
-    var anchors = $("a:contains("+ muscularGroups[0] +")");
-
-    request()
-
-    console.log(anchors);
+function addToCollection(woutRoutine){
 
 
-})
+}
 
 
 

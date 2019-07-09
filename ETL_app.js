5bf7d3a047f4b145f2f60b4351290c93412dedfa
FranCali <fr.califano@gmail.com>
2019-07-09 15:39:21 +0200

    added setup functions to ETL_app

--- ETL_app.js
+++ ETL_app.js
@@ -1,7 +1,13 @@
 const foodETL = require('./src/foodETL');
 const exercise = require('./src/exercise');
+var equipment = require('./src/equipment');
 const MongoClient = require('mongodb').MongoClient;
 const urlDB = 'mongodb://localhost:27017/';
 
-foodETL.performETL();
+//foodETL.performETL();
+
 exercise.exerciseHandler(MongoClient, urlDB);
+
+//Setup Equipment Collection and Populate Products
+equipment.initEquipmentCollection(MongoClient, urlDB, "com", "relevanceblender", "1");
+

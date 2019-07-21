099044e0c9c8c15ceb54f2021898e9ccad4df8d9
Pietro Russo <ruoppolo94@hotmail.it>
2019-07-21 19:57:57 +0200

    Added Ebay API and some bug-fixing

--- src/equipment.js
+++ src/equipment.js
@@ -1,44 +1,45 @@
 var unirest = require('unirest');
 var request = require('sync-request');
 
-
-exports.initEquipmentCollection = function(MongoClient, urlDB, domainCode, sortBy, page) {
+exports.initEquipmentCollection = function(MongoClient, urlDB) {
 
   console.log("EQUIPMENT: Creating Equipment Collection...");
   var created = createEquipmentCollection(MongoClient, urlDB);
   created.then(function(result) {
-    populateEquipmentCollection(MongoClient, urlDB, domainCode, sortBy, page);
+    populateEquipmentCollection(MongoClient, urlDB);
   }, function(err) {
     console.log(err)
   });
 }
 
-exports.updateEquipmentCollection = function(MongoClient, urlDB, domainCode, sortBy, page) {
+exports.updateEquipmentCollection = function(MongoClient, urlDB) {
 
   console.log("EQUIPMENT: Updating Equipment Collection...");
-  populateEquipmentCollection(MongoClient, urlDB, domainCode, sortBy, page);
+  populateEquipmentCollection(MongoClient, urlDB);
 }
 
-exports.findByKeywordAmz = function(MongoClient, urlDB, domainCode, keyword, sortBy, page) {
+exports.findByKeyword = function(MongoClient, urlDB, keyword) {
   
   return new Promise(function(fulfill, reject) {
 
-    var lookupResult = lookupByKeywordAmz(MongoClient, urlDB, keyword);
+    var lookupResult = lookupByKeyword(MongoClient, urlDB, keyword);
     lookupResult.then(function(result) {
       fulfill(result);
     },
     function (err) {
       //console.log("Nothing found for '" + keyword + "' in DB.");
-      var APIResult = searchByKeywordAmz(MongoClient, urlDB, domainCode, keyword, sortBy, page);
-      APIResult.then(function(result) {
-        var lookupResult = lookupByKeywordAmz(MongoClient, urlDB, keyword);
-        lookupResult.then(function(result) {
-          fulfill(result);
+      var AmzResult = searchByKeywordAmz(MongoClient, urlDB, keyword);
+      AmzResult.then(function(result) {
+        var EbayResult = searchByKeywordEbay(MongoClient, urlDB, keyword);
+        EbayResult.then(function(result) {
+          var lookupResult = lookupByKeyword(MongoClient, urlDB, keyword);
+          lookupResult.then(function(result) {
+            fulfill(result);
+          });
         });
       });
     });
   });
-
 }
 
 function createEquipmentCollection(MongoClient, urlDB) {
@@ -74,7 +75,7 @@ function createEquipmentCollection(MongoClient, urlDB) {
   })
 }
 
-function populateEquipmentCollection(MongoClient, urlDB, domainCode, sortBy, page) {
+function populateEquipmentCollection(MongoClient, urlDB) {
 
   //console.log("Populating Equipment Collection...");
   // Populate Equipment Collection
@@ -88,7 +89,8 @@ function populateEquipmentCollection(MongoClient, urlDB, domainCode, sortBy, pag
         equipment.forEach(eq => {
           if(eq.name != "none (bodyweight exercise)") {
             //console.log("Found equipment: '" + eq.name + "'.");
-            searchByKeywordAmz(MongoClient, urlDB, domainCode, eq.name, sortBy, page);
+            searchByKeywordAmz(MongoClient, urlDB, eq.name);
+            searchByKeywordEbay(MongoClient, urlDB, eq.name);
           }
         });
         db.close();
@@ -98,7 +100,7 @@ function populateEquipmentCollection(MongoClient, urlDB, domainCode, sortBy, pag
   });
 }
 
-function lookupByKeywordAmz(MongoClient, urlDB, keyword) {
+function lookupByKeyword(MongoClient, urlDB, keyword) {
 
   //console.log("Looking up for '" + keyword + "' in DB...");
   return new Promise(function (fulfill, reject){
@@ -107,19 +109,19 @@ function lookupByKeywordAmz(MongoClient, urlDB, keyword) {
         throw err;
       else {
         var dbo = db.db("Fit_AdvisorDB");
-        dbo.collection("Equipment").findOne({name : keyword},  { _id : 0, amazonProducts : 1}, function(err, found) {
+        dbo.collection("Equipment").findOne({name : keyword},  { _id : 0, offers : 1}, function(err, found) {
           if(err) {
             console.log(err);
             reject(err);
           }
-          if(found.amazonProducts != undefined) {
+          if(found.offers != undefined) {
             //console.log("Found '" + keyword + "' in DB!");
-            found.amazonProducts.sort(function(p1,p2) {
-              if(p1.price < p2.price) return -1
+            found.offers.sort(function(p1,p2) {
+              if(parseInt(p1.price) < parseInt(p2.price)) return -1
               else return +1
             });
             db.close();
-            fulfill(found.amazonProducts);
+            fulfill(found.offers);
           }
           reject(false);
         });
@@ -128,7 +130,7 @@ function lookupByKeywordAmz(MongoClient, urlDB, keyword) {
   });
 }
 
-function searchByKeywordAmz(MongoClient, urlDB, domainCode, keyword, sortBy, page) {
+function searchByKeywordAmz(MongoClient, urlDB, keyword) {
 
   //console.log("Axesso API Request for '" + keyword + "'...");
 
@@ -138,6 +140,9 @@ function searchByKeywordAmz(MongoClient, urlDB, domainCode, keyword, sortBy, pag
   return new Promise(function (fulfill, reject) {
 
     var requestType = "amazon-search-by-keyword";
+    var domainCode = "com";
+    var sortBy = "relevanceblender";
+    var page = "2";
 
   //Axesso Product Request
   unirest.get("https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/"+ requestType +"?sortBy="+ sortBy +"&domainCode=" + domainCode +"&page=" + page +"&keyword=" + keyword)
@@ -151,29 +156,139 @@ function searchByKeywordAmz(MongoClient, urlDB, domainCode, keyword, sortBy, pag
 
 
       //console.log("Found Products for '" + keyword + "'...");
-      updateProducts(result, keyword, MongoClient, urlDB);
+      
+      //Update Equipment Collection Offers
+      MongoClient.connect(urlDB, { useNewUrlParser: true },function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("Fit_AdvisorDB");
+        var offers = [];
+        var equipmentSearch = JSON.parse(JSON.stringify(result.body)).foundProductDetails;
+
+        equipmentSearch.forEach(eq => {
+
+          let offer = {
+            id: eq.asin,
+            name: eq.productTitle,
+            producer: eq.manufacturer,
+            reviews: eq.countReview,
+            rating: eq.productRating,
+            seller: eq.soldBy,
+            price: eq.price,
+            image: eq.imageUrlList[0],
+            itemLink: "https://www.amazon.com/dp/" + eq.asin,
+            marketplace: "amazon"
+          };
+          offers.push(offer);
+          
+        });
+
+        //console.log("Updating '" + keyword + "' in DB...");
+
+        dbo.collection("Equipment").updateMany(
+          {"name" : keyword},
+          {$addToSet: {"offers": {$each: offers}}}
+        );
+        //console.log("'" + keyword + "' Equipment Collection created");
+        db.close();
+      });
+      
       fulfill(true);
 
     });
   });
 }
 
-function updateProducts(result, keyword, MongoClient, urlDB) {
-  
-  //Update MongoDB Equipment Collection Products (Cache)
-  MongoClient.connect(urlDB, { useNewUrlParser: true },function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("Fit_AdvisorDB");
-    var equipmentSearch = JSON.parse(JSON.stringify(result.body)).foundProductDetails;
+function searchByKeywordEbay(MongoClient, urlDB, keyword) {
+
+  var SECURITY_APPNAME = "PietroRu-FitAdvis-PRD-fd8d7989f-4f00e3ee";
+  var SERVICE_VERSION = "1.0.0";
+  var OPERATION_NAME = "findItemsAdvanced"
+  var GLOBAL_ID = "EBAY-US";
+  var RESPONSE_DATA_FORMAT = "JSON";
+  var entries = "10";
+
+  //console.log("Ebay API Request for '" + keyword + "'...");
+
+  if(keyword == "Bench") keyword = "Flat bench";
+  if(keyword == "EZ-Bar") keyword = "Ez curl bar";
+
+  return new Promise(function (fulfill, reject) {
 
 
-    //console.log("Updating '" + keyword + "' in DB...");
+    //Ebay findItemsAdvanced Request
+    unirest.get("https://svcs.ebay.com/services/search/FindingService/v1" +
+    "?keywords=" + keyword +
+    "&paginationInput.entriesPerPage=" + entries +
+    "&outputSelector(0)=SellerInfo")
+      .header("X-EBAY-SOA-GLOBAL-ID" , GLOBAL_ID)
+      .header("X-EBAY-SOA-SECURITY-APPNAME", SECURITY_APPNAME)
+      .header("X-EBAY-SOA-SERVICE-VERSION", SERVICE_VERSION)
+      .header("X-EBAY-SOA-OPERATION-NAME", OPERATION_NAME)
+      .header("X-EBAY-SOA-RESPONSE-DATA-FORMAT", RESPONSE_DATA_FORMAT)
+      .end(function(result) {
 
-    dbo.collection("Equipment").updateMany(
-      {"name" : keyword},
-      {$set: {"amazonProducts": equipmentSearch}}
-    );
-    //console.log("'" + keyword + "' Equipment Collection created");
-    db.close();
+        if(keyword == "Flat bench") keyword = "Bench";
+        if(keyword == "Ez curl bar") keyword = "EZ-Bar";
+        
+        //Update Equipment Collection Offers
+        MongoClient.connect(urlDB, { useNewUrlParser: true },function (err, db) {
+          if (err) throw err;
+          var dbo = db.db("Fit_AdvisorDB");
+          var offers = [];
+          
+          //console.log(findAndCorrect(JSON.parse(result.body)).findItemsAdvancedResponse.searchResult.item);
+          var equipmentSearch = findAndCorrect(JSON.parse(result.body)).findItemsAdvancedResponse.searchResult.item;
+
+          equipmentSearch.forEach(eq => {
+
+            let offer = {
+              id: eq.itemId[0],
+              name: eq.title[0], 
+              producer: "Not Found",
+              reviews: "Not Found",
+              rating: "Not Found",
+              seller: eq.sellerInfo[0].sellerUserName[0],
+              price: eq.sellingStatus[0].currentPrice[0].__value__,
+              image: eq.galleryURL[0],
+              itemLink: eq.viewItemURL[0],
+              marketplace: "ebay"
+            };
+
+            offers.push(offer);
+          });
+
+          //console.log("Updating '" + keyword + "' in DB...");
+          dbo.collection("Equipment").updateMany(
+            {"name" : keyword},
+            {$addToSet: {"offers": {$each: offers}}}
+          );
+
+          //console.log("'" + keyword + "' Equipment Collection created");
+          db.close();
+
+          fulfill(true);
+        });
+    });
   });
+}
+
+function findAndCorrect(obj){
+
+  let o = obj,
+      exclu = ['item'];
+
+  for (const key in o) {
+      if (o.hasOwnProperty(key)) {
+          const val = o[key];
+
+          if(Array.isArray(val) && val.length == 1){
+              if(exclu.indexOf(key) == -1)
+                  o[key] = val[0];
+              o[key] = findAndCorrect(o[key]);
+          }else if(!Array.isArray(val) && typeof val == 'object'){                        
+              o[key] = findAndCorrect(val);
+          }
+      }
+  }
+  return o;
 }
